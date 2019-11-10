9e2c0ba1cc8b7c7328460a75172cbcc336654a9e
Thomas Andersen <thomas.andersen77@gmail.com>
2019-11-10 19:10:24 +0100

    late init var autowired

--- src/test/java/no/knowit/spring/springtutorial/data/MeldingRepositoryTest.kt
+++ src/test/java/no/knowit/spring/springtutorial/data/MeldingRepositoryTest.kt
@@ -1,22 +1,20 @@
 package no.knowit.spring.springtutorial.data
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
-import kotlin.test.assertTrue
-
 @DataJpaTest
 open class MeldingRepositoryTest {
     @Autowired
-    private val repository: MeldingRepository? = null
+    private lateinit var repository: MeldingRepository
 
     @Test
     fun findByType() {
-        assertTrue { repository!!.save(Melding("", Type.INTERN)).id!! > 0 }
-        val melding = repository!!.findByType(Type.INTERN)
+        assertTrue { repository.save(Melding("", Type.INTERN)).id!! > 0 }
+        val melding = repository.findByType(Type.INTERN)
         assertTrue( melding.hasId() )
         assertEquals(Type.INTERN, melding.type)
     }
